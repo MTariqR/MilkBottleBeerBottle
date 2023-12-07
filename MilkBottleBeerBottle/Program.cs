26dac4355f7cf3e7de166dd5c7dc78765a3519bf
MTariqR <5100155@mypillar7uwc.ac.za>
2023-12-07 11:01:36 +0200

    finished it i guess

--- MilkBottleBeerBottle/Program.cs
+++ MilkBottleBeerBottle/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int amount = 99;
+            string bottles = "bottles";
+            while (amount > 0)
+            {
+                if (amount == 1)
+                {
+                    bottles = "bottle";
+                    Console.WriteLine($"{amount} {bottles} of beer on the wall, {amount} {bottles}  of beer.");
+                    break;
+                }
+                Console.WriteLine($"{amount} {bottles} of beer on the wall, {amount} {bottles}  of beer.\nTake one down, and pass it around, " +
+                    $"{amount-1} {bottles} of beer on the wall.");
+                amount--;
+            }
         }
     }
 }
